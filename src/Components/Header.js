eaf559ccbf8fc5a70946941a2bd75d4d55710bf4
Alauddin97 <alen.maxx17@gmail.com>
2019-07-24 00:37:18 +0530

    first try

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './css/header.css'
+
+function Header() {
+    return (
+        <div className="header">
+           <b> My Website</b>
+        </div>
+    )
+}
+
+export default Header
+
---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './css/navbar.css'
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            
+                <b className="b">Home</b>
+                <b className="b">About</b>
+                <b className="b">Services</b>
+                <b className="b">Contacts</b>
+        </div>
+    )
+}
+
+export default Navbar
